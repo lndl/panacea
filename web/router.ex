5dca600b850a2edc92292ed03712c06f72d8b979
Lautaro Nahuel De León <laudleon@gmail.com>
2017-06-14 23:31:46 -0300

    Refactor en routers

--- web/router.ex
+++ web/router.ex
@@ -1,26 +1,14 @@
 defmodule Prueba.Router do
   use Prueba.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", Prueba do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
-  end
-
-   scope "/api", Prueba do
+   scope "/", Prueba do
      pipe_through :api
      resources "/events", EventController, except: [:new, :edit]
+
+     get "/", EventController, :index
    end
 end
